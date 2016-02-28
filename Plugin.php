a97d9c9f2ed054c3736ebea90ba42d1e3f59a0e7
scottbedard <scottbedard90@gmail.com>
2016-02-28 16:01:00 -0700

    Add youtube tags

--- Plugin.php
+++ Plugin.php
@@ -1,5 +1,6 @@
 <?php namespace BeEasy\BlogExtension;
 
+use Event;
 use Backend;
 use System\Classes\PluginBase;
 use RainLab\Blog\Models\Post;
@@ -34,7 +35,17 @@ class Plugin extends PluginBase
 
     public function boot()
     {
-        // Extend the controller
+        $this->extendPostsController();
+        $this->extendMarkdown();
+    }
+
+    /**
+     * Extend the PostsController
+     *
+     * @return void
+     */
+    protected function extendPostsController()
+    {
         PostsController::extendFormFields(function($form, $model, $context) {
             if (!$model instanceof Post) return;
             $form->addSecondaryTabFields([
@@ -44,6 +55,22 @@ class Plugin extends PluginBase
                     'default' => 'View post',
                 ],
             ]);
+        });
+    }
+
+    /**
+     * Add custom markdown tags
+     *
+     * @return void
+     */
+    protected function extendMarkdown()
+    {
+        Event::listen('markdown.beforeParse', function($data) {
+            $data->text = preg_replace(
+                '/\<(?:yt|youtube)\s*=\s*\"?(\w+)\"?\s*\/?\>/im',
+                '<div class="youtube-wrapper"><iframe type="text/html" src="http://www.youtube.com/embed/$1" frameborder="0"/></iframe></div>',
+                $data->text
+            );
         });
     }
 }
