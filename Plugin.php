eb6c060ca8a7b487e4c358eca5b59787640aeaf9
scottbedard <scottbedard90@gmail.com>
2016-02-28 23:01:39 -0700

    Markup helpers

--- Plugin.php
+++ Plugin.php
@@ -66,9 +66,25 @@ class Plugin extends PluginBase
     protected function extendMarkdown()
     {
         Event::listen('markdown.beforeParse', function($data) {
+
+            // YouTube
             $data->text = preg_replace(
-                '/\<(?:yt|youtube)\s*=\s*\"?(\w+)\"?\s*\/?\>/im',
-                '<div class="youtube-wrapper"><iframe type="text/html" src="http://www.youtube.com/embed/$1" frameborder="0"/></iframe></div>',
+                '/\<(?:youtube)\s*=\s*\"?(\w+)\"?\s*\/?\>/im',
+                '<div class="video-wrapper"><iframe type="text/html" src="http://www.youtube.com/embed/$1" frameborder="0"/></iframe></div>',
+                $data->text
+            );
+
+            // Vimeo
+            $data->text = preg_replace(
+                '/\<(?:vimeo)\s*=\s*\"?(\w+)\"?\s*\/?\>/im',
+                '<div class="video-wrapper"><iframe src="//player.vimeo.com/video/$1" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>',
+                $data->text
+            );
+
+            // Replace <left>, <center>, and <right> tags with <div>
+            $data->text = preg_replace(
+                '/\<(left|center|right)\>(.*)\<\/\1\>/im',
+                '<div style="text-align: $1">$2</div>',
                 $data->text
             );
         });
